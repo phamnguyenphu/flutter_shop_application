1ef3d0b9db2543ec3d87b4b608fb7d9b833572eb
Pham Nguyen Phu <phamnguyenphucp9@gmail.com>
2021-11-06 15:50:56 +0700

    detailScreen
    xây dựng trang Detail Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop_application/providers/auth.dart';
 import 'package:flutter_shop_application/providers/cart.dart';
+import 'package:flutter_shop_application/providers/product.dart';
 import 'package:flutter_shop_application/screens/auth_screen.dart';
 import 'package:flutter_shop_application/screens/cart_screen.dart';
 import 'package:flutter_shop_application/screens/drawer_screen.dart';
@@ -53,80 +54,81 @@ class MyApp extends StatelessWidget {
                 previousOrder!..update(auth.token, auth.userId)),
       ],
       child: Consumer<Auth>(
-        builder: (ctx, auth, _) =>
-            Sizer(builder: (context, orientation, deviceType) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'MyShop',
-            theme: ThemeData(
-              canvasColor: Colors.white,
-              primarySwatch: Colors.orange,
-              // ignore: deprecated_member_use
-              accentColor: Colors.orange,
-              fontFamily: 'Lato',
-              textTheme: TextTheme(
-                  subtitle1: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.0,
-                  ),
-                  bodyText1: TextStyle(
-                      fontSize: 16,
+        builder: (ctx, auth, _) => Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'MyShop',
+              theme: ThemeData(
+                canvasColor: Colors.white,
+                primarySwatch: Colors.orange,
+                // ignore: deprecated_member_use
+                accentColor: Colors.orange,
+                fontFamily: 'Lato',
+                textTheme: TextTheme(
+                    subtitle1: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0,
+                    ),
+                    bodyText1: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 0.5,
+                        fontWeight: FontWeight.bold),
+                    subtitle2: TextStyle(
+                      fontSize: 13,
                       letterSpacing: 0.5,
-                      fontWeight: FontWeight.bold),
-                  subtitle2: TextStyle(
-                    fontSize: 13,
-                    letterSpacing: 0.5,
-                  ),
-                  headline4: TextStyle(
-                    fontSize: 28,
-                    letterSpacing: 0.5,
-                  ),
-                  headline3: TextStyle(
-                    fontSize: 15,
-                  )),
-              appBarTheme: AppBarTheme(
-                color: Colors.white,
-                elevation: 0.0,
-                centerTitle: true,
+                    ),
+                    headline4: TextStyle(
+                      fontSize: 28,
+                      letterSpacing: 0.5,
+                    ),
+                    headline3: TextStyle(
+                      fontSize: 15,
+                    )),
+                appBarTheme: AppBarTheme(
+                  color: Colors.white,
+                  elevation: 0.0,
+                  centerTitle: true,
+                ),
               ),
-            ),
-            home: auth.isSplash
-                ? (initScreen == 0 || initScreen == null)
-                    ? SplashScreen()
-                    : auth.isAuth
-                        ? Scaffold(
-                            body: Stack(
-                              children: [
-                                DrawerScreen(),
-                                ProductsOverviewScreen(),
-                              ],
-                            ),
-                          )
-                        : AuthenScreen()
-                : auth.isAuth
-                    ? Scaffold(
-                        body: Stack(
-                          children: [
-                            DrawerScreen(),
-                            ProductsOverviewScreen(),
-                          ],
-                        ),
-                      )
-                    : AuthenScreen(),
-            routes: {
-              ProductsOverviewScreen.routeName: (ctx) =>
-                  ProductsOverviewScreen(),
-              AuthenScreen.routeName: (ctx) => AuthenScreen(),
-              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-              CartScreen.routeName: (ctx) => CartScreen(),
-              OrderScreen.routeName: (ctx) => OrderScreen(),
-              UserProductScreen.routeName: (ctx) => UserProductScreen(),
-              EditProductScreen.routeName: (ctx) => EditProductScreen(),
-              SplashScreen.routeName: (ctx) => SplashScreen(),
-            },
-          );
-        }),
+              home: auth.isSplash
+                  ? (initScreen == 0 || initScreen == null)
+                      ? SplashScreen()
+                      : auth.isAuth
+                          ? Scaffold(
+                              body: Stack(
+                                children: [
+                                  DrawerScreen(),
+                                  ProductsOverviewScreen(),
+                                ],
+                              ),
+                            )
+                          : AuthenScreen()
+                  : auth.isAuth
+                      ? Scaffold(
+                          body: Stack(
+                            children: [
+                              DrawerScreen(),
+                              ProductsOverviewScreen(),
+                            ],
+                          ),
+                        )
+                      : AuthenScreen(),
+              routes: {
+                ProductsOverviewScreen.routeName: (ctx) =>
+                    ProductsOverviewScreen(),
+                AuthenScreen.routeName: (ctx) => AuthenScreen(),
+                ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+                CartScreen.routeName: (ctx) => CartScreen(),
+                OrderScreen.routeName: (ctx) => OrderScreen(),
+                UserProductScreen.routeName: (ctx) => UserProductScreen(),
+                EditProductScreen.routeName: (ctx) => EditProductScreen(),
+                SplashScreen.routeName: (ctx) => SplashScreen(),
+              },
+            );
+          },
+        ),
       ),
     );
   }
