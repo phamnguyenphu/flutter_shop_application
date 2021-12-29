5a36fdeb32b5f52b800fca956c186c3d4b68c0af
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2021-12-29 14:39:23 +0700

    [FIX_UI] Drawer
    Fix UI drawer
    BorderRadius

--- lib/screens/drawer_screen.dart
+++ lib/screens/drawer_screen.dart
@@ -45,174 +45,168 @@ class _DrawerScreenState extends State<DrawerScreen> {
         : Container(
             width: MediaQuery.of(context).size.width * 0.75,
             child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20.0),
-                    bottomRight: Radius.circular(100.0)),
-                child: Drawer(
-                  child: Container(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(20.0),
-                              bottomRight: Radius.circular(20.0)),
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.3,
-                            width: double.infinity,
-                            color: Color.fromRGBO(143, 148, 251, 1),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      user.avatar == ''
-                                          ? 'https://firebasestorage.googleapis.com/v0/b/flutter-shop-d0a51.appspot.com/o/avatar.jpg?alt=media&token=cdb54cc3-6514-4e4f-b69b-8794450d2da3'
-                                          : user.avatar,
-                                      scale: 1.0),
-                                  radius: 50.0,
-                                ),
-                                SizedBox(
-                                  height: 10.0,
-                                ),
-                                Text(
-                                  user.fullName == '' ? 'Guest' : user.fullName,
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                ),
-                                SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                    user.email == ''
-                                        ? 'Guest@guest.com'
-                                        : user.email,
-                                    style:
-                                        Theme.of(context).textTheme.subtitle2),
-                              ],
-                            ),
+              child: Drawer(
+                child: Container(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20.0),
+                            bottomRight: Radius.circular(20.0)),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          width: double.infinity,
+                          color: Color.fromRGBO(143, 148, 251, 1),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    user.avatar == ''
+                                        ? 'https://firebasestorage.googleapis.com/v0/b/flutter-shop-d0a51.appspot.com/o/avatar.jpg?alt=media&token=cdb54cc3-6514-4e4f-b69b-8794450d2da3'
+                                        : user.avatar,
+                                    scale: 1.0),
+                                radius: 50.0,
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                user.fullName == '' ? 'Guest' : user.fullName,
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Text(
+                                  user.email == ''
+                                      ? 'Guest@guest.com'
+                                      : user.email,
+                                  style: Theme.of(context).textTheme.subtitle2),
+                            ],
                           ),
                         ),
-                        user.email == ''
-                            ? Container(
-                                margin: EdgeInsets.only(top: 40.0),
-                                child: Column(children: [
+                      ),
+                      user.email == ''
+                          ? Container(
+                              margin: EdgeInsets.only(top: 40.0),
+                              child: Column(children: [
+                                ListTile(
+                                  onTap: () {
+                                    Provider.of<Auth>(context, listen: false)
+                                        .logOut();
+                                    Provider.of<User>(context, listen: false)
+                                        .logout();
+                                  },
+                                  leading: Icon(Icons.lock),
+                                  title: Text('Login'),
+                                ),
+                                ListTile(
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed(AboutUsScreen.routeName);
+                                  },
+                                  leading: Icon(Icons.info),
+                                  title: Text('About'),
+                                ),
+                              ]))
+                          : Container(
+                              margin: EdgeInsets.only(top: 40.0),
+                              child: Column(
+                                children: [
                                   ListTile(
                                     onTap: () {
-                                      Provider.of<Auth>(context, listen: false)
-                                          .logOut();
-                                      Provider.of<User>(context, listen: false)
-                                          .logout();
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ProfileScreen(
+                                                    email: user.email,
+                                                    isSignUp: false,
+                                                  )));
                                     },
-                                    leading: Icon(Icons.lock),
-                                    title: Text('Login'),
+                                    leading: Icon(Icons.account_box),
+                                    title: Text('Profile'),
+                                  ),
+                                  ListTile(
+                                    onTap: () {
+                                      Navigator.of(context).push(CustomRoute(
+                                          builder: (ctx) => OrderScreen()));
+                                    },
+                                    leading: Icon(Icons.task),
+                                    title: Text('Order'),
                                   ),
                                   ListTile(
                                     onTap: () {
                                       Navigator.of(context)
-                                          .pushNamed(AboutUsScreen.routeName);
+                                          .pushNamed(SettingScreen.routeName);
                                     },
-                                    leading: Icon(Icons.info),
-                                    title: Text('About'),
+                                    leading: Icon(Icons.settings),
+                                    title: Text('Languages'),
                                   ),
-                                ]))
-                            : Container(
-                                margin: EdgeInsets.only(top: 40.0),
-                                child: Column(
-                                  children: [
-                                    ListTile(
-                                      onTap: () {
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    ProfileScreen(
-                                                      email: user.email,
-                                                      isSignUp: false,
-                                                    )));
-                                      },
-                                      leading: Icon(Icons.account_box),
-                                      title: Text('Profile'),
-                                    ),
-                                    ListTile(
+                                  ListTile(
+                                      leading: Icon(Icons.info),
+                                      title: Text('About us'),
                                       onTap: () {
                                         Navigator.of(context).push(CustomRoute(
-                                            builder: (ctx) => OrderScreen()));
-                                      },
-                                      leading: Icon(Icons.task),
-                                      title: Text('Order'),
-                                    ),
-                                    ListTile(
-                                      onTap: () {
-                                        Navigator.of(context)
-                                            .pushNamed(SettingScreen.routeName);
-                                      },
-                                      leading: Icon(Icons.settings),
-                                      title: Text('Languages'),
-                                    ),
-                                    ListTile(
-                                        leading: Icon(Icons.info),
-                                        title: Text('About us'),
-                                        onTap: () {
-                                          Navigator.of(context).push(
-                                              CustomRoute(
-                                                  builder: (ctx) =>
-                                                      AboutUsScreen()));
-                                        }),
-                                    ListTile(
-                                      leading: Icon(Icons.logout),
-                                      title: Text('Log out'),
-                                      onTap: () {
-                                        showDialog(
-                                            context: context,
-                                            builder: (ctx) => AlertDialog(
-                                                  elevation: 5.0,
-                                                  backgroundColor: Colors.white,
-                                                  title: Row(
-                                                    children: [
-                                                      Icon(
-                                                        Icons.warning_rounded,
-                                                        size: 20.0,
-                                                        color: Colors.red,
-                                                      ),
-                                                      SizedBox(
-                                                        width: 15.0,
-                                                      ),
-                                                      Text('Are you sure?')
-                                                    ],
-                                                  ),
-                                                  content: Text(
-                                                      'Do you want to sign out of the app?'),
-                                                  actions: [
-                                                    // ignore: deprecated_member_use
-                                                    FlatButton(
-                                                        onPressed: () {
-                                                          Navigator.of(context)
-                                                              .pop(false);
-                                                        },
-                                                        child:
-                                                            const Text('No')),
-                                                    // ignore: deprecated_member_use
-                                                    FlatButton(
-                                                        onPressed: () {
-                                                          Provider.of<Auth>(
-                                                                  context,
-                                                                  listen: false)
-                                                              .logOut();
-                                                          Provider.of<User>(
-                                                                  context,
-                                                                  listen: false)
-                                                              .logout();
-                                                          Navigator.of(context)
-                                                              .pop(false);
-                                                        },
-                                                        child:
-                                                            const Text('Yes')),
+                                            builder: (ctx) => AboutUsScreen()));
+                                      }),
+                                  ListTile(
+                                    leading: Icon(Icons.logout),
+                                    title: Text('Log out'),
+                                    onTap: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (ctx) => AlertDialog(
+                                                elevation: 5.0,
+                                                backgroundColor: Colors.white,
+                                                title: Row(
+                                                  children: [
+                                                    Icon(
+                                                      Icons.warning_rounded,
+                                                      size: 20.0,
+                                                      color: Colors.red,
+                                                    ),
+                                                    SizedBox(
+                                                      width: 15.0,
+                                                    ),
+                                                    Text('Are you sure?')
                                                   ],
-                                                ));
-                                      },
-                                    ),
-                                  ],
-                                ))
-                      ])),
-                )));
+                                                ),
+                                                content: Text(
+                                                    'Do you want to sign out of the app?'),
+                                                actions: [
+                                                  // ignore: deprecated_member_use
+                                                  FlatButton(
+                                                      onPressed: () {
+                                                        Navigator.of(context)
+                                                            .pop(false);
+                                                      },
+                                                      child: const Text('No')),
+                                                  // ignore: deprecated_member_use
+                                                  FlatButton(
+                                                      onPressed: () {
+                                                        Provider.of<Auth>(
+                                                                context,
+                                                                listen: false)
+                                                            .logOut();
+                                                        Provider.of<User>(
+                                                                context,
+                                                                listen: false)
+                                                            .logout();
+                                                        Navigator.of(context)
+                                                            .pop(false);
+                                                      },
+                                                      child: const Text('Yes')),
+                                                ],
+                                              ));
+                                    },
+                                  ),
+                                ],
+                              ))
+                    ])),
+              ),
+            ),
+          );
   }
 }
